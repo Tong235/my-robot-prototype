f20b40a437ae0ff3bf7128fbdcccef813afa3102
Tong235 <114546892+Tong235@users.noreply.github.com>
2022-10-12 10:05:07 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,5 @@
+basic.forever(function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.soaring), SoundExpressionPlayMode.UntilDone)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.twinkle), SoundExpressionPlayMode.UntilDone)
+})
